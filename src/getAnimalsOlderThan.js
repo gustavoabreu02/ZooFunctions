cd2a8a1b58c7e2407ed9271dc373391acc15481a
Gustavo Abreu <gustavofart371@gmail.com>
2022-04-22 10:31:59 -0300

    Implementa a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animais = species.filter((value) => animal === value.name);
+  return animais[animais.length - 1].residents.every((value) => value.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
