0e4a5c41da6132017c110b781e548559318c7b6c
Gustavo Abreu <gustavofart371@gmail.com>
2022-04-22 12:22:02 -0300

    Implementa a função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+  const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+  const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+  if (id === stephanieId || id === olaId || id === burlId) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  try {
+    if (isManager(managerId) === false) {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    }
+    if (isManager(managerId) === true) {
+      const func = employees.filter((value) => value.managers[0] === managerId);
+      return func.map((value) => `${value.firstName} ${value.lastName}`);
+    }
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
