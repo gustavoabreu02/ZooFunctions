402b75ad5a78169e5dab0b737c4755a1ce06cf5a
Gustavo Abreu <gustavofart371@gmail.com>
2022-04-22 10:50:15 -0300

    Implementa a função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqu
+  const userDisplay = employees.find((value) => (
+    value.firstName === employeeName || value.lastName === employeeName));
+  if (typeof userDisplay === 'object') {
+    return userDisplay;
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
