305b6c9dbaa929deee4e486db64fc3e3aad18706
Gustavo Abreu <gustavofart371@gmail.com>
2022-04-24 02:15:09 -0300

    Implementa a função getOldestFromFirstSpecies

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  let resultado = {
+    NE: ['lions', 'giraffes'],
+    NW: ['tigers', 'bears', 'elephants'],
+    SE: ['penguins', 'otters'],
+    SW: ['frogs', 'snakes'],
+  };
+  const animaisNE = species.filter((value) => value.location === 'NE');
+  const NEA = animaisNE.map((value) => value.name).name;
+  if (options.includeNames === true) {
+    resultado = { NE: NEA, NW: [], SE: [], SW: [] };
+  }
+  return resultado;
 }
+
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const resultado = employees.find((value) => value.id === id);
+  const animal = species.find((value) => value.id === resultado.responsibleFor[0]);
+  const idadeAnimal = animal.residents.map((value) => value.age);
+  const AnimalVelho = idadeAnimal.reduce((acc, value) => {
+    if (acc > value) {
+      return acc;
+    }
+    return value;
+  });
+  const animalFormato = animal.residents.find((value) => value.age === AnimalVelho);
+  return Object.values(animalFormato);
 }
 
 module.exports = getOldestFromFirstSpecies;
